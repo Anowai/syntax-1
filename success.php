0d6e3397d676386b2e3dbbc1db3d7a6a6736662b
Anowai.py <anowaijames@gmail.com>
2019-09-17 15:15:57 +0100

    Added more features to the success page and changed the go back text to sign out button

--- success.php
+++ success.php
@@ -12,18 +12,19 @@ if(!isset($_SESSION['login_user'])){
 
     <meta charset="UTF-8">
 
-    <title>Success</title>
+    <title>Team Syntax</title>
 
 </head>
 
 <body>
+	<h1 style="text-align:center; color: blue">WELCOME TO TEAM SYNTAX</h1>
 
     <div id="container">
     <h2 style="text-align:center; color: blue">You are logged in Successfully <?php echo $_SESSION['login_user']?> !</h2>
   </div>
 
    <div >
-<a href="index.php"><p style="text-transform:none;text-align:right; color: red"><<< Go back</p></a>
+<a href="index.php"><button style="text-transform:none;text-align:right; color: red">Sign Out</button></a>
   </div>
 </body>
 
